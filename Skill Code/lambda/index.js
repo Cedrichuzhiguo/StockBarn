a84771ad26f9b1729475f23e7507186ab737eb79
argoo <admin@argoo.ca>
2020-05-03 23:03:34 -0400

    final clean of previous dead code

--- Skill Code/lambda/index.js
+++ Skill Code/lambda/index.js
@@ -386,19 +386,7 @@ const ErrorHandler = {
 
 /* CONSTANTS */
 const skillBuilder = Alexa.SkillBuilders.custom();
-const imagePath = "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/{0}x{1}/{2}._TTH_.png";
-const backgroundImagePath = "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/{0}x{1}/{2}._TTH_.png"
-const speechConsCorrect = ['Booya', 'All righty', 'Bam', 'Bazinga', 'Bingo', 'Boom', 'Bravo', 'Cha Ching', 'Cheers', 'Dynomite', 'Hip hip hooray', 'Hurrah', 'Hurray', 'Huzzah', 'Oh dear.  Just kidding.  Hurray', 'Kaboom', 'Kaching', 'Oh snap', 'Phew','Righto', 'Way to go', 'Well done', 'Whee', 'Woo hoo', 'Yay', 'Wowza', 'Yowsa'];
-const speechConsWrong = ['Argh', 'Aw man', 'Blarg', 'Blast', 'Boo', 'Bummer', 'Darn', "D'oh", 'Dun dun dun', 'Eek', 'Honk', 'Le sigh', 'Mamma mia', 'Oh boy', 'Oh dear', 'Oof', 'Ouch', 'Ruh roh', 'Shucks', 'Uh oh', 'Wah wah', 'Whoops a daisy', 'Yikes'];
-const data = [
-  {StateName: 'Alabama', Abbreviation: 'AL', Capital: 'Montgomery', StatehoodYear: 1819, StatehoodOrder: 22},
-  {StateName: 'Alaska', Abbreviation: 'AK', Capital: 'Juneau', StatehoodYear: 1959, StatehoodOrder: 49}
-];
 
-const states = {
-  START: `_START`,
-  QUIZ: `_QUIZ`,
-};
 
 const attr_name = {
     REDIRECT_INTENT: 'REDIRECT_INTENT',
@@ -410,7 +398,7 @@ const suggestAccountLink = 'Do you think I\'m helpful enough? I can help you eve
 const exitSkillMessage = `Thank you for using stock ninja! You'll make a lot money soon!`;
 const promptForAddingStock = `To add a stock, you can say Amazon, or A-M-Z-N`;
 const helpMessage = `I am stock ninja, help you manage your portofolio. For example, you can ask, check price of amazon.`;
-const useCardsFlag = true;
+
 
 /* HELPER FUNCTIONS */
 
@@ -449,96 +437,12 @@ function supportsDisplay(handlerInput) {
 }
 
 
-function getBadAnswer(item) {
-  return `I'm sorry. ${item} is not something I know very much about in this skill. ${helpMessage}`;
-}
-
-function getCurrentScore(score, counter) {
-  return `Your current score is ${score} out of ${counter}. `;
-}
-
-function getFinalScore(score, counter) {
-  return `Your final score is ${score} out of ${counter}. `;
-}
-
-function getCardTitle(item) {
-  return item.StateName;
-}
-
-function getSmallImage(item) {
-  return `https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/720x400/${item.Abbreviation}._TTH_.png`;
-}
-
-function getLargeImage(item) {
-  return `https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/1200x800/${item.Abbreviation}._TTH_.png`;
-}
-
-function getImage(height, width, label) {
-  return imagePath.replace("{0}", height)
-    .replace("{1}", width)
-    .replace("{2}", label);
-}
-
-function getBackgroundImage(label, height = 1024, width = 600) {
-  return backgroundImagePath.replace("{0}", height)
-    .replace("{1}", width)
-    .replace("{2}", label);
-}
-
-function getSpeechDescription(item) {
-  return `${item.StateName} is the ${item.StatehoodOrder} state, admitted to the Union in ${item.StatehoodYear}.  The capital of ${item.StateName} is ${item.Capital}, and the abbreviation for ${item.StateName} is <break strength='strong'/><say-as interpret-as='spell-out'>${item.Abbreviation}</say-as>.  I've added ${item.StateName} to your Alexa app.  Which other state or capital would you like to know about?`;
-}
-
-function formatCasing(key) {
-  return key.split(/(?=[A-Z])/).join(' ');
-}
-
-function getQuestion(counter, property, item) {
-  return `Here is your ${counter}th question.  What is the ${formatCasing(property)} of ${item.StateName}?`;
-}
-
-// getQuestionWithoutOrdinal returns the question without the ordinal and is
-// used for the echo show.
-function getQuestionWithoutOrdinal(property, item) {
-  return "What is the " + formatCasing(property).toLowerCase() + " of "  + item.StateName + "?";
-}
-
-function getAnswer(property, item) {
-  switch (property) {
-    case 'Abbreviation':
-      return `The ${formatCasing(property)} of ${item.StateName} is <say-as interpret-as='spell-out'>${item[property]}</say-as>. `;
-    default:
-      return `The ${formatCasing(property)} of ${item.StateName} is ${item[property]}. `;
-  }
-}
 
 function getRandom(min, max) {
   return Math.floor((Math.random() * ((max - min) + 1)) + min);
 }
 
-function askQuestion(handlerInput) {
-  console.log("I am in askQuestion()");
-  //GENERATING THE RANDOM QUESTION FROM DATA
-  const random = getRandom(0, data.length - 1);
-  const item = data[random];
-  const propertyArray = Object.getOwnPropertyNames(item);
-  const property = propertyArray[getRandom(1, propertyArray.length - 1)];
 
-  //GET SESSION ATTRIBUTES
-  const attributes = handlerInput.attributesManager.getSessionAttributes();
-
-  //SET QUESTION DATA TO ATTRIBUTES
-  attributes.selectedItemIndex = random;
-  attributes.quizItem = item;
-  attributes.quizProperty = property;
-  attributes.counter += 1;
-
-  //SAVE ATTRIBUTES
-  handlerInput.attributesManager.setSessionAttributes(attributes);
-
-  const question = getQuestion(attributes.counter, property, item);
-  return question;
-}
 //Check whether session attribute 'attributeName' has a value 'value'.
 function isAttributeEqualsToValue(handlerInput, attributeName, value){
   const attributes = handlerInput.attributesManager.getSessionAttributes();
@@ -613,32 +517,6 @@ function compareSlots(slots, value) {
   }
 
   return false;
-}
-
-function getItem(slots) {
-  const propertyArray = Object.getOwnPropertyNames(data[0]);
-  let slotValue;
-
-  for (const slot in slots) {
-    if (Object.prototype.hasOwnProperty.call(slots, slot) && slots[slot].value !== undefined) {
-      slotValue = slots[slot].value;
-      for (const property in propertyArray) {
-        if (Object.prototype.hasOwnProperty.call(propertyArray, property)) {
-          const item = data.filter(x => x[propertyArray[property]]
-            .toString().toLowerCase() === slots[slot].value.toString().toLowerCase());
-          if (item.length > 0) {
-            return item[0];
-          }
-        }
-      }
-    }
-  }
-  return slotValue;
-}
-
-function getSpeechCon(type) {
-  if (type) return `<say-as interpret-as='interjection'>${speechConsCorrect[getRandom(0, speechConsCorrect.length - 1)]}! </say-as><break strength='strong'/>`;
-  return `<say-as interpret-as='interjection'>${speechConsWrong[getRandom(0, speechConsWrong.length - 1)]} </say-as><break strength='strong'/>`;
 }
 
 
